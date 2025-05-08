5eaeacf177d37ae6c112d0313dca8758f72ba653
ParkerS00 <parkerswenson00@gmail.com>
2025-05-08 11:06:42 -0600

    Some quick tests to prove the basics of dto conversion

---
+++ BudgetPro.Test/DTOTests.cs
@@ -0,0 +1,79 @@
+﻿using BudgetPro.Server.DTOs;
+using Shouldly;
+
+namespace BudgetPro.Test;
+
+public class DTOTests
+{
+    [Fact]
+    public void CanMakeABudgetDTO()
+    {
+        // Arrange
+        var testBudget = DefaultFactory.CreateTestBudget();
+
+        // Act
+        var testDTO = testBudget.ToDTO();
+
+        // Assert
+        testDTO.Amount.ShouldBe(testBudget.Amount);
+        testDTO.Category.ShouldNotBeNull();
+        testDTO.CategoryId.ShouldBe(testBudget.CategoryId);
+        testDTO.Id.ShouldBe(testBudget.Id);
+        testDTO.TimeFrame.ShouldBe(testBudget.Timeframe);
+        testDTO.User.ShouldNotBeNull();
+        testDTO.UserId.ShouldBe(testBudget.UserId);
+    }
+
+    [Fact]
+    public void CanMakeUserDTO()
+    {
+        // Arrange
+        var testUser = DefaultFactory.CreateTestUser();
+
+        // Act
+        var testDTO = testUser.ToDTO();
+
+        // Assert
+        testDTO.Email.ShouldBe(testUser.Email);
+        testDTO.Id.ShouldBe(testUser.Id);
+        testDTO.CreatedAt.ShouldBe(testUser.CreatedAt);
+    }
+
+    [Fact]
+    public void CanMakeCategoryDTO()
+    {
+        // Arrange
+        var testCategory = DefaultFactory.CreateTestCategory();
+
+        // Act
+        var testDTO = testCategory.ToDTO();
+
+        // Assert
+        testDTO.Id.ShouldBe(testCategory.Id);
+        testDTO.IsIncome.ShouldBe(testCategory.IsIncome);
+        testDTO.Name.ShouldBe(testCategory.Name);
+        testDTO.User.ShouldNotBeNull();
+        testDTO.UserId.ShouldBe(testCategory.UserId);
+    }
+
+    [Fact]
+    public void CanMakeTransactionDTO()
+    {
+        // Arrange
+        var testTransaction = DefaultFactory.CreateTestTransaction();
+
+        // Act
+        var testDTO = testTransaction.ToDTO();
+
+        // Assert
+        testDTO.Amount.ShouldBe(testTransaction.Amount);
+        testDTO.Category.ShouldNotBeNull();
+        testDTO.CategoryId.ShouldBe(testTransaction.CategoryId);
+        testDTO.DateOccured.ShouldBe(testTransaction.DateOccured);
+        testDTO.Description.ShouldBe(testTransaction.Description);
+        testDTO.Id.ShouldBe(testTransaction.Id);
+        testDTO.IsRecurring.ShouldBe(testTransaction.IsRecurring);
+        testDTO.User.ShouldNotBeNull();
+        testDTO.UserId.ShouldBe(testTransaction.UserId);
+    }
+}
---
+++ BudgetPro.Test/DefaultFactory.cs
@@ -0,0 +1,58 @@
+﻿using BudgetPro.Server.Data;
+
+namespace BudgetPro.Test;
+
+public static class DefaultFactory
+{
+    public static Budget CreateTestBudget()
+    {
+        return new Budget()
+        {
+            Amount = 100,
+            Category = new Category(),
+            CategoryId = 1,
+            Id = 1,
+            Timeframe = new DateOnly(2025, 5, 8),
+            User = new User(),
+            UserId = 1
+        };
+    }
+
+    public static User CreateTestUser()
+    {
+        return new User()
+        {
+            CreatedAt = new DateTime(2025, 5, 8),
+            Email = "test@gmail.com",
+            Id = 1
+        };
+    }
+
+    public static Category CreateTestCategory()
+    {
+        return new Category()
+        {
+            Id = 1,
+            IsIncome = true,
+            Name = "Salary",
+            User = new User(),
+            UserId = 1
+        };
+    }
+
+    public static Transaction CreateTestTransaction()
+    {
+        return new Transaction()
+        {
+            Amount = 100,
+            Category = new Category(),
+            CategoryId = 1,
+            DateOccured = new DateOnly(2025, 5, 8),
+            Description = "Test Description",
+            Id = 1,
+            IsRecurring = true,
+            User = new User(),
+            UserId = 1
+        };
+    }
+}
