0449681d32721a370bda63ac683a1ed5b826e1e6
ParkerS00 <parkerswenson00@gmail.com>
2025-05-08 20:30:22 -0600

    Navbar looks bad who did this

--- budgetpro.client/src/app/app.module.ts
+++ budgetpro.client/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @NgModule({
   declarations: [
@@ -11,8 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule, HttpClientModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    NavbarComponent
+],
   providers: [],
   bootstrap: [AppComponent]
 })
